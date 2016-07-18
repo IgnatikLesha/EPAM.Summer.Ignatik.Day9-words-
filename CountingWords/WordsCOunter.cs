e9079116f19cf680dacb176b62679cd45584c0c3
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-18 18:07:30 +0300

    Add project files.

---
+++ CountingWords/WordsCOunter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingWords
+{
+    public class WordsCounter
+    {
+        public Dictionary<string, int> CountWordsInFile(string file)
+        {
+            if (!File.Exists(file))
+                throw new FileNotFoundException();
+            string tempString = File.ReadAllText(file);
+            return CountOfWords(tempString);
+        }
+
+        private static Dictionary<string, int> CountOfWords(string str)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            string[] tempStrArray = str.Split(' ', ',', '.', ':', '!', '?', '\t');
+
+            foreach (var word in tempStrArray)
+            {
+                if (dictionary.ContainsKey(word))
+                    dictionary[word]++;
+                else
+                    dictionary.Add(word, 1);
+            }
+            return dictionary;
+        }
+    }
+}
